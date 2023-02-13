d4a09587d05407d51ae03f1c33897e7110408f4a
jayed-phero <jayed.programminghero@gmail.com>
2023-02-13 01:06:14 -0800

    commiit

--- src/Pages/Home/NewsUpdate/NewsUpdate.js
+++ src/Pages/Home/NewsUpdate/NewsUpdate.js
@@ -4,16 +4,16 @@ import './NewsUpdate.css';
 const NewsUpdate = () => {
     return (
         <div className='flex items-start flex-col lg:flex-row max-w-7xl mx-auto'>
-            <div className='flex-1 py-16'>
+            <div className='py-16 flex-1'>
                 <div className='flex items-center gap-7 pb-2'>
-                    <h3 className='text-2xl md:text-3xl font-bold text-regal-indigo'>News & Updates</h3>
-                    {/* <p className='h-2 w-full bg-gray-400 '></p> */}
+                    <h3 className='text-2xl md:text-3xl font-bold text-regal-indigo '>News & Updates</h3>
+                    {/* <div className='border-2 border-gray-500 w-full'></div> */}
                 </div>
                 <p className='text-regal-green mb-5'>Read All News</p>
                 <div className='flex items-start flex-col md:flex-row gap-9'>
-                    <div className=''>
+                    <div className='w-[450px]'>
                         <img src='https://images.unsplash.com/photo-1491841550275-ad7854e35ca6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8ZWR1Y2F0aW9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=2000&q=60'
-                            className='w-full h-56'
+                            className='w-full  h-56'
                         />
                         <div className='mt-7'>
                             <div className='flex items-center gap-3 text-gray-400 pb-3'>
@@ -21,11 +21,14 @@ const NewsUpdate = () => {
                                 <h3>ADMISSION STUDENT </h3>
                             </div>
                             <h3 className='font-bold text-2xl hover:text-regal-green text-gray-700'>Professor Albert joint research on mobile money in Tanzania</h3>
+                            <button className='bg-regal-green w-44 py-3 hover:bg-green-600  mt-5 flex items-center justify-center font-bold text-white '>
+                                See Details
+                            </button>
                         </div>
                     </div>
                     <div>
                         {
-                            [1, 2, 3].map(i =>
+                            [1, 2, 3, 4].map(i =>
                                 <div className='flex items-center gap-5 mb-5'>
                                     <img src='https://images.unsplash.com/photo-1491841550275-ad7854e35ca6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8ZWR1Y2F0aW9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=2000&q=60'
                                         className='imageST rounded '
@@ -43,11 +46,11 @@ const NewsUpdate = () => {
                     </div>
                 </div>
             </div>
-            <div className='lg:w-[400px] w-full bg-regal-indigo px-9 py-16 my-16'>
-                <h3 className='text-2xl md:text-3xl font-bold text-regal-indigo'>News & Updates</h3>
+            <div className='lg:w-[400px] w-full bg-regal-indigo px-9 py-16'>
+                <h3 className='text-2xl md:text-3xl font-bold text-white pb-5'>Quick Links</h3>
                 {
                     [1, 2, 3, 4, 5, 6, 7].map(i =>
-                        <h3 className='text-xl font-semibold pb-2 border-b-2 border-gray-400 mb-5 text-regal-green'>research mobile money</h3>
+                        <h3 className='text-xl font-semibold pb-5 border-b-2 border-gray-400 my-5 text-regal-green'>research mobile money</h3>
                     )
                 }
             </div>
--- src/Pages/Home/ShortInfo/ShortInfo.js
+++ src/Pages/Home/ShortInfo/ShortInfo.js
@@ -7,22 +7,26 @@ const ShortInfo = () => {
         {
             icon: <i class="fa-solid fa-chalkboard-user"></i>,
             number: "300",
-            title: "Teachers"
+            title: "Teachers of TMKMT",
+            desc: "Kingster University was established by John Smith in 1920 for the public benefit and it is recognized."
         },
         {
             icon: <i class="fa-solid fa-school"></i>,
             number: "79",
-            title: "Classrooms"
+            title: "Classrooms Number",
+            desc: "Kingster University was established by John Smith in 1920 for the public benefit and it is recognized."
         },
         {
             icon: <i class="fa-solid fa-graduation-cap"></i>,
             number: "11900",
-            title: "Students"
+            title: "Students In TMKMT",
+            desc: "Kingster University was established by John Smith in 1920 for the public benefit and it is recognized."
         },
         {
             icon: <i className="fa-solid fa-building-columns"></i>,
             number: "1985",
-            title: "Founded"
+            title: "Founded Year",
+            desc: "Kingster University was established by John Smith in 1920 for the public benefit and it is recognized."
         },
     ]
     return (
@@ -30,10 +34,11 @@ const ShortInfo = () => {
             <div className='grid grid-cols-1 md:grid-cols-2  lg:grid-cols-4 gap-16 py-20  px-5  max-w-7xl mx-auto mt-20'>
                 {
                     shortDetails.map((details, i) =>
-                        <div key={i} className='flex items-center flex-col justify-center border-dotted border-2 border-white  rounded-3xl relative w-full py-12 '>
-                            <p className='text-7xl text-regal-green pb-2'>{details.icon}</p>
-                            <h3 className='text-white text-xl font-semibold'>{details.title}</h3>
-                            <h1 className='bg-white w-32 py-2 flex items-center justify-center absolute -bottom-6 text-3xl font-bold text-black '>
+                        <div key={i} className=''>
+                            <p className='text-5xl text-regal-green py-3'>{details.icon}</p>
+                            <h3 className='text-white text-2xl font-semibold pb-3'>{details.title}</h3>
+                            <h3 className='text-gray-400  '>{details.desc}</h3>
+                            <h1 className='border-regal-green border-2 w-32 py-2 rounded-xl mt-5 flex items-center justify-center text-3xl font-bold text-white '>
                                 <CountUp end={details.number} duration={5} enableScrollSpy />
                             </h1>
                         </div>
